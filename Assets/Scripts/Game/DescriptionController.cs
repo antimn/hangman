20338d668821d4a6a7e1ac9a6e34d3b665edfdd8
Sergei Blockiy <sergei.blockiy@gmail.com>
2023-02-20 11:45:34 +0300

    Add description for code

--- Assets/Scripts/Game/DescriptionController.cs
+++ Assets/Scripts/Game/DescriptionController.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 
 public class DescriptionController : MonoBehaviour
 {
-   [SerializeField] private TextMeshProUGUI _descriptionText;
+    [SerializeField] private TextMeshProUGUI _descriptionText;
 
-   public void SetDescription(string description)
-   {
-      _descriptionText.text = description;
-   }
+    public void SetDescription(string description)
+    {
+        _descriptionText.text = description;
+    }
 }
+
+// В скрипте есть одно поле:
+// _descriptionText типа TextMeshProUGUI, которое используется для отображения текста в игре.В методе SetDescription
+// передается строка description, которая задает текст для отображения.Этот метод присваивает заданный текст
+// _descriptionText, чтобы отобразить его в игре.
--- Assets/Scripts/Game/GameLost.cs
+++ Assets/Scripts/Game/GameLost.cs
@@ -12,11 +12,17 @@ namespace HangmanClass.Scripts
         {
             print("You Lost!");
             if (_loseText != null) _loseText.text = "You Lost!";
-            
+
             // Делаем кнопку рестарта активной
             restartButton.SetActive(true);
 
             // Отключаем возможность ввода слов
+            // Этот код используется для отключения компонента GameManager в Unity.
+            // Сначала он находит объект с компонентом GameManager с помощью метода FindObjectOfType().Затем он
+            // устанавливает значение свойства enabled этого компонента в значение false, что приводит к
+            // тому, что компонент больше не обрабатывает события в игр
+            // Таким образом, если GameManager ранее управлял логикой игры, то после выполнения этого кода игрок не сможет
+            // взаимодействовать с игрой, так как его логика более не обрабатывается.
             GameManager gameManager = FindObjectOfType<GameManager>();
             gameManager.enabled = false;
         }
